4bed5485d00a7880a217875474d16f15ca06b368
zsuzsihudacsko <zsuzsi.hudacsko@gmail.com>
2021-04-19 13:54:42 +0200

    added packages and hempty homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Redirect, Route, Router, Switch} from "react-router-dom";
+import {createBrowserHistory} from 'history'
+import HomePage from "./components/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const history = createBrowserHistory();
+
+function App(): JSX.Element {
+    return (
+        <div>
+            <Router history={history}>
+                <Switch>
+                    <Route path='/home'><HomePage/></Route>
+                    <Route path='/'><Redirect to="/home"/></Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomePage(): JSX.Element {
+    return (
+        <div>
+            <p>Hello Home!</p>
+        </div>
+    );
+}
+
+export default HomePage;
